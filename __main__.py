fd782ed36e1e4d442e07d68e47438580bb275959
Zoraman26 <88414058+Zoraman26@users.noreply.github.com>
2022-08-24 19:08:23 -0500

    intial commit

---
+++ __main__.py
@@ -0,0 +1,53 @@
+import webview
+import os
+
+
+class Api:
+    def destroy(self):
+        print('Destroying window..')
+        window.destroy()
+        print('Destroyed!')
+
+    def test(self):
+        print('Test')
+
+
+    def folderselect(self):
+        result = window.create_file_dialog(webview.FOLDER_DIALOG, allow_multiple=False)
+        path = result
+        print(path)
+        file = path[0] + '\pack.mcmeta'
+        if os.path.exists(file):
+            print('Valid pack')
+            window.evaluate_js(
+                r"""
+                showValidPackButton()
+                """
+            )
+        else:
+            print('Invalid pack')
+            print('Displaying error')
+            window.evaluate_js(
+                r"""
+                alert('This is not a valid resource pack');
+                """
+            )
+        showpathinhtml = window.evaluate_js(
+            r"""
+            showResponse({message: ${path}})
+            """
+        )
+
+
+    def showResponse(self):
+        window.evaluate_js(
+            r"""
+            showResponse({message: path})
+            """
+        )
+
+
+if __name__ == '__main__':
+    api = Api()
+    window = webview.create_window('Resource', 'assets/main.html', js_api=api, min_size=(300, 100))
+webview.start(debug=True)
